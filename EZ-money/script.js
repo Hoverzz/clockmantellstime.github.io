81c1c1c830451824b3b8d70a9f7e3560dc0efc28
ClockmanTellsTime <87088337+ClockmanTellsTime@users.noreply.github.com>
2021-10-21 18:02:43 -0700

    script.js

--- EZ-money/script.js
+++ EZ-money/script.js
@@ -1,8 +1,33 @@
-
 document.addEventListener("keyup", function(event) {
+    console.log(event.keyCode + " - " + event.key);
     if (event.keyCode === 49) {
-        upgrade();
+        if (document.getElementById("game").style.visibility === "visible"){
+            upgrade();
+        }
+        else {
+            if (document.getElementById("difficulty").style.visibility === "visible"){
+                setDifficulty("easy");  
+            } 
+            else {
+                start();
+            } 
+        }
     }
+    if (event.keyCode === 50) {
+        if (document.getElementById("difficulty").style.visibility === "visible"){
+            setDifficulty("medium");  
+        }  
+    } 
+    if (event.keyCode === 51) {
+        if (document.getElementById("difficulty").style.visibility === "visible"){
+            setDifficulty("hard");  
+        }  
+    }      
+    if (event.keyCode === 52) {
+        if (document.getElementById("difficulty").style.visibility === "visible"){
+            setDifficulty("insane");  
+        }  
+    }   
     if (event.keyCode === 83) {
         if (document.getElementById("game").style.visibility === "visible"){
             saveGame();
@@ -10,7 +35,6 @@ document.addEventListener("keyup", function(event) {
         }
     }
     if (event.keyCode === 82) {
-        
         if (document.getElementById("game").style.visibility === "visible"){
             resetGame();
         }
@@ -21,10 +45,8 @@ document.addEventListener("keyup", function(event) {
 });
                 
 let stats = {
-    
     money: 0,
     upgrade: 1,
-    pretige: 0,
 
     printer: 0,
     builder: 0,
@@ -54,9 +76,12 @@ let stats = {
 
 }
 
+let other = {
+    diffiiculty: 0,
+}
+
 let prices = {
     upgrade: 1000,
-    prestige: 1000000000000000000,
 
     printer: 1000,
     builder: 25000,
@@ -70,6 +95,13 @@ let prices = {
 
     has: 10000,
     has1: 100000
+}
+
+let difficulties = {
+    "easy": 1,
+    "medium": 2,
+    "hard": 5,
+    "insane": 20
 }
 
 let automations = {
@@ -209,11 +241,11 @@ function upgrade() {
 }
 
 function num2txt(num) {
-    const suffixes = [" thousand"," million"," billion"," trillion"," quadrillion"," quintillion"," sextillion"," septillion", " octillion"," nontillion"," dectillion"];
+    const suffixes = [" thousand"," million"," billion"," trillion"," quadrillion"," quintillion"];
     let n = String(num);
     let txt = 0
     
-    if (num > 999) {
+    if (num > 999 && num < 10000000000000000000) {
         if ((n.length-1) % 3 === 0) {
             let first = n.charAt(0);
             let second = n.charAt(1);
@@ -259,8 +291,8 @@ function reload() {
     document.getElementById("mk1").innerHTML = "Mk1 factory: A basic factory that prints $"+num2txt(stats.mk1Earn)+" every second, costs " + num2txt(prices.mk1) + "!";
     document.getElementById("mk2").innerHTML = "Mk2 factory: a factory that can print $"+num2txt(stats.mk2Earn)+" every second, costs " + num2txt(prices.mk2) + "!"; 
     document.getElementById("mk3").innerHTML = "Mk3 factory: a slightly advanced factory that can print $"+num2txt(stats.mk3Earn)+" every second, costs " + num2txt(prices.mk3) + "!";
-    document.getElementById("mk4").innerHTML = "Mk4 factory: an advanced factory that can produce $"+num2txt(stats.mk4Earn)+" every second, costs a whopping " + num2txt(prices.mk4) + "!";
-    document.getElementById("mk5").innerHTML = "Mk5 factory: A super edvanced factory that can print $"+num2txt(stats.mk5Earn) +" every second, but it isn't cheap!";
+    document.getElementById("mk4").innerHTML = "Mk4 factory: an advanced factory that can produce $"+num2txt(stats.mk4Earn)+" every second, costs " + num2txt(prices.mk4) + "!";
+    document.getElementById("mk5").innerHTML = "Mk5 factory: A super advanced factory that can print $"+num2txt(stats.mk5Earn) +" every second, but it isn't cheap!";
 }     
 
 function autoUpgrade() {
@@ -335,47 +367,58 @@ function levelUp(thing) {
 
 function start() {
     if (confirm("Are you sure?")) {
-        document.getElementById("game").style.visibility = "visible";
+        document.getElementById("difficulty").style.visibility = "visible";
         document.getElementById("start").style.visibility = "hidden";
-        document.getElementById("body").style.backgroundColor = "white";
+        document.getElementById("body").style.backgroundColor = "black";
         document.getElementById("start1").style.visibility = "hidden";
     }
     
 }
 
+function setDifficulty(diffiiculty) {
+    if (confirm("Are you sure, you can't change the difficulty later?")) {
+        for (var p in prices) {
+            prices[p]  *=  difficulties[diffiiculty];
+        }
+        other.diffiiculty = diffiiculty;
+        document.getElementById("difficulty").style.visibility = "hidden";
+        document.getElementById("game").style.visibility = "visible";
+        document.getElementById("body").style.backgroundColor = "white";
+    }
+}
+
 function saveGame() {
-        
-    localStorage.setItem("stats",JSON.stringify(stats));
+    if (other.diffiiculty === "easy"){
+        localStorage.setItem("easy",JSON.stringify(stats));
+    }
+    if (other.diffiiculty === "medium"){
+        localStorage.setItem("medium",JSON.stringify(stats));
+    }
+    if (other.diffiiculty === "hard"){
+        localStorage.setItem("hard",JSON.stringify(stats));
+    }
+    if (other.diffiiculty === "easy"){
+        localStorage.setItem("insane",JSON.stringify(stats));
+    }
 }
 
 function loadGame() {
-    var data = JSON.parse(localStorage.getItem("stats"));
+    if (other.diffiiculty === "easy"){
+        var data = JSON.parse(localStorage.getItem("easy"));
+    }
+    if (other.diffiiculty === "medium"){
+        var data = JSON.parse(localStorage.getItem("medium"));
+    }    
+    if (other.diffiiculty === "hard"){
+        var data = JSON.parse(localStorage.getItem("hard"));
+    }  
+    if (other.diffiiculty === "insane"){
+        var data = JSON.parse(localStorage.getItem("insane"));  
+    }    
     try{
-        stats.money = data.money;
-        stats.upgrade = data.upgrade;
-        stats.printer = data.printer;
-        stats.builder = data.builder;
-        stats.manager = data.manager;
-        stats.mk1 = data.mk1;
-        stats.mk2 = data.mk2
-        stats.mk3 = data.mk3
-        stats.mk4 = data.mk4
-        stats.mk5 = data.mk5
-        stats.mk1Lvl = data.mk1Lvl
-        stats.mk2Lvl = data.mk2Lvl
-        stats.mk3Lvl = data.mk3Lvl
-        stats.mk4Lvl = data.mk4Lvl
-        stats.mk5Lvl = data.mk5Lvl                        
-        stats.earnPrinter = data.earnPrinter
-        stats.mk1Earn = data.mk1Earn
-        stats.mk2Earn = data.mk2Earn
-        stats.mk3Earn = data.mk3Earn
-        stats.mk4Earn = data.mk4Earn
-        stats.mk5Earn = data.mk5Earn
-        stats.has = data.has
-        stats.has1 = data.has1
-
-        
+        for (var x in data) {
+            stats[x] = data[x];
+        }
     }
     catch(error){
         window.alert("no save found!")
@@ -416,17 +459,19 @@ function resetGame() {
             has1: false,
 
         }
-        localStorage.setItem("stats",JSON.stringify(stats));
+        if (other.diffiiculty === "easy"){
+            localStorage.setItem("easy",JSON.stringify(stats));
+        }
+        if (other.diffiiculty === "medium"){
+            localStorage.setItem("medium",JSON.stringify(stats));
+        }
+        if (other.diffiiculty === "hard"){
+            localStorage.setItem("hard",JSON.stringify(stats));
+        }
+        if (other.diffiiculty === "insane"){
+            localStorage.setItem("insane",JSON.stringify(stats));
+        }
         loadGame();
     }   
 }
 
-function prestige() {
-    if (stats.money > prices.prestige * prestige) {
-        stats.mk1Earn = math.floor((stats.mk1Earn * (1 + (2*Math.random))))
-        stats.mk2Earn = math.floor((stats.mk2Earn * (1 + (2*Math.random))))
-        stats.mk3Earn = math.floor((stats.mk3Earn * (1 + (2*Math.random))))
-        stats.mk4Earn = math.floor((stats.mk4Earn * (1 + (2*Math.random))))
-        stats.mk5Earn = math.floor((stats.mk5Earn * (1 + (2*Math.random))))
-    }
-}
