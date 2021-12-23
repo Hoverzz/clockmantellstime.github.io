7323df0c1b4ee5448e51574a2020ec0233dd3776
ClockmanTellsTime <87088337+ClockmanTellsTime@users.noreply.github.com>
2021-12-22 21:09:29 -0800

    Update script.js

--- EZ-money-2/script.js
+++ EZ-money-2/script.js
@@ -3,8 +3,11 @@ document.addEventListener("keydown",function(event){
     if (event.keyCode == 46) {
         reset();
     }
+    if (event.keyCode == 191) {
+        game.stats.money += (game.stats.multiplier) * ((game.stats.rebirth + game.stats.super_rebirth + game.stats.ultra_rebirth + game.stats.prestige + game.stats.super_prestige + game.stats.ultra_prestige + game.stats.power + game.stats.super_power + game.stats.ultra_power + game.stats.god)+1)
+        wth()
+    }
 })
-
 var game = {
     stats: {    
         money: 0,
@@ -19,7 +22,8 @@ var game = {
         super_power: 0,
         ultra_power: 0,
         god: 0,
-        null: 0
+        null: 0,
+        StartScreen: true
     },
     prices: {
         rebirth: 5000,
@@ -32,16 +36,25 @@ var game = {
         super_power: 5000,
         ultra_power: 8000,
         god: 1000000
-
     },
     things: ["money","multiplier","rebirth","super_rebirth","ultra_rebirth","prestige","super_prestige","ultra_prestige","power","super_power","ultra_power","god","null"],
-    max: 100000000000000000000
+    max: 100000000000000000000,
 }
 
 const formatter = new Intl.NumberFormat('en', {
     notation: 'compact'
 })
 
+
+function fix(char) {
+    if (char.includes("_")) {
+        var x = char.replace("_"," ")
+    }
+    else {
+        var x = char;
+    }
+    return x 
+}
 
 function update() {
     document.getElementById("money").innerHTML = "money: $" + formatter.format(game.stats.money);
@@ -56,54 +69,50 @@ function update() {
     document.getElementById("super_power").innerHTML = "super power: " + formatter.format(game.stats.super_power);
     document.getElementById("ultra_power").innerHTML = "ultra power: " + formatter.format(game.stats.ultra_power);
     document.getElementById("god").innerHTML = "gods: " + formatter.format(game.stats.god);
-    
 }
 
 function start() {
-    if (confirm("Are you sure?")) {
-        document.getElementById("startScreen").style.visibility = "hidden";
-        document.getElementById("stats").style.visibility = "visible"
-        document.getElementById("buttons").style.visibility = "visible"
-        document.getElementById("body").style.backgroundColor = "white";
-        document.getElementById("body").style.animation = "sports 5s 0s infinite alternate none";
+    document.getElementById("startScreen").style.visibility = "hidden";
+    document.getElementById("stats").style.visibility = "visible"
+    document.getElementById("buttons").style.visibility = "visible"
+    document.getElementById("body").style.backgroundColor = "white";
+    document.getElementById("body").style.animation = "sports 5s 0s infinite alternate none";
+    update();
+    var child = document.getElementById("startScreen")
+    child.parentNode.removeChild(child)
+    setInterval(() => {
+        game.stats.money += game.stats.multiplier
+    }, 100);
+    setInterval(() => {
+        game.stats.multiplier += (game.stats.rebirth + 1) * (game.stats.god + 1);
+    }, 100);
+    
+    setInterval(() => {
         update();
-        var child = document.getElementById("startScreen")
-        child.parentNode.removeChild(child)
-        setInterval(() => {
-            game.stats.money += game.stats.multiplier
-        }, 100);
-        setInterval(() => {
-            game.stats.multiplier += (game.stats.rebirth + 1) * (game.stats.god + 1);
-        }, 100);
-        
-        setInterval(() => {
-            update();
-        }, 100);
-        setInterval(() => {
-            save();
-        }, 100);
-        
-    }
+    }, 100);
+    setInterval(() => {
+        save();
+    }, 100);
 }
 
 function upgrade(thing) {
     if (thing == "rebirth") {
         if (game.stats.money >= game.prices.rebirth) {
-            if (game.stats.rebirth >= 100000000000000000000) {
+            if (game.stats.rebirth >= game.max) {
                 game.stats.rebirth += 1
                 game.stats.money = 0;
                 game.stats.multiplier = 0;
             }
             else {
                 var amount = Math.floor(game.stats.money/game.prices.rebirth);
-                if (amount >= 100000000000000000000) {
-                    game.stats.rebirth += 100000000000000000000 - game.stats.rebirth
+                if (amount >= game.max) {
+                    game.stats.rebirth += game.max - game.stats.rebirth
                     game.stats.money = 0;
                     game.stats.multiplier = 0;                    
                 }
                 else {
-                    if (amount * (game.stats.super_rebirth + 1) > 100000000000000000000) {
-                        game.stats.rebirth += 100000000000000000000 - game.stats.rebirth
+                    if (amount * (game.stats.super_rebirth + 1) > game.max) {
+                        game.stats.rebirth += game.max - game.stats.rebirth
                     }
                     else {
                         game.stats.rebirth += amount * (game.stats.super_rebirth + 1)
@@ -114,7 +123,11 @@ function upgrade(thing) {
             }
             
         }
+        else {
+            window.alert("You need $" + formatter.format(game.prices.rebirth - game.stats.money) + " more to rebirth!")
+        }
     }
+    
     else {
         for (var x in game.things) {
             if (game.things[x] == thing) {
@@ -128,7 +141,7 @@ function upgrade(thing) {
                 break;
             }
         }
-        if (game.stats[thing] >= 100000000000000000000) {
+        if (game.stats[thing] >= game.max) {
             game.stats[thing] += 1;
             for (var x in game.things) {
                 if (game.things[x] == thing) {
@@ -143,13 +156,13 @@ function upgrade(thing) {
         else {
             if (game.stats[OneDown] >= game.prices[thing]) {
                 var amount = Math.floor(game.stats[OneDown]/game.prices[thing]) 
-                if (amount > 100000000000000000000) {
-                    game.stats[thing] += 100000000000000000000 - game.stats[thing]
+                if (amount > game.max) {
+                    game.stats[thing] += game.max - game.stats[thing]
                 }
                 else {
                     var thiss = game.stats[game.things[game.things.indexOf(thing) + 1]] + 1
-                    if (amount * thiss >= 100000000000000000000) {
-                        game.stats[thing] += 100000000000000000000 - game.stats[thing]
+                    if (amount * thiss >= game.max) {
+                        game.stats[thing] += game.max - game.stats[thing]
                     }
                     else {
                         game.stats[thing] += amount * thiss
@@ -164,6 +177,14 @@ function upgrade(thing) {
                         }
                     }
                 }
+            }
+            else {
+                if (game.prices[thing]-game.stats[OneDown] === 1) {
+                    window.alert("You need " + formatter.format(game.prices[thing]-game.stats[OneDown]) + " more " + fix(OneDown) + " to do this!")
+                }
+                else {
+                    window.alert("You need " + formatter.format(game.prices[thing]-game.stats[OneDown]) + " more " + fix(OneDown) + "s to do this!")
+                }  
             }
         }
     }
@@ -214,8 +235,7 @@ function reset() {
     
         },
         things: ["money","multiplier","rebirth","super_rebirth","ultra_rebirth","prestige","super_prestige","ultra_prestige","power","super_power","ultra_power","god","null"],
-        max: 100000000000000000000
+        max: game.max
     }
     
 }
-
